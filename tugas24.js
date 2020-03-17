a4a75b3619f04debb6194879509e914401cab09e
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:30:46 +0700

    tugas niomic

---
+++ tugas24.js
@@ -0,0 +1,8 @@
+function tugas24(){
+    let x = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+    console.log("Sebelumnya : " + x);
+    console.log("Ascending : " + x.sort());
+    console.log("Descending : " + x.reverse(x.sort()));
+};
+
+tugas24();
